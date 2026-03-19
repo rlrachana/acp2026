5ac8c6d494d10eeb376ed81288623a8659f436c6
rlrachana <rlrachana737@gmail.com>
2026-03-19 19:43:31 +0530

    demo1 compiled successfully

--- level1/demo1.c
+++ level1/demo1.c
@@ -3,16 +3,21 @@ void input(int *n,float arr[])
 {
     printf("Enter total number of values:");
     scanf("%d",n);
+    if(*n<=0)
+    {
+        printf("Invalid value of n");
+        return ;
+    }
     printf("Enter %d values:",*n);
     for(int i=0;i<*n;i++)
     {
-        scanf("%f",&arr[i]);
+    scanf("%f",&arr[i]);
     }
 }
 int find_max_index(int n,float arr[])
 {
     int max_index=0;
-    for(int i=0;i<n;i++)
+    for(int i=1;i<n;i++)
     {
         if(arr[i]>arr[max_index])
         {
@@ -30,6 +35,10 @@ int main()
     int n,max_index;
     float arr[100];
     input(&n,arr);
+    if(n<=0)
+    {
+        return 0;
+    }
     max_index=find_max_index(n,arr);
     output(arr,max_index);
     return 0;
