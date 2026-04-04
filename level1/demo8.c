c3dc9ee35ce1f4ac93d4b9b561eaf6a256243660
rlrachana <rlrachana737@gmail.com>
2026-04-04 23:42:57 +0530

    demo5

--- level1/demo8.c
+++ level1/demo8.c
@@ -1,25 +1,39 @@
-#include<stdio.h>
-#include<string.h>
-void input(char *str)
-{
+#include <stdio.h>
+void input(char *str) {
+    printf("Enter string:\n");
     scanf("%s",str);
 }
-void concatenate_strings(char *str1, char *str2)
+
+void concatenate_strings(char *str1, char *str2) 
 {
-    strcat(str1,str2);
+    int i=0,j=0;
+    while(str1[i]!='\0')
+    {
+        i++;
+    }
+    while(str2[j]!='\0')
+    {
+        str1[i]=str2[j];
+        i++;
+        j++;
+    }
+    str1[i]='\0';
 }
-void display(char *str)
-{
-    printf("String after concatenation:%s",str);
+
+void display(char *str) {
+    printf("Concatenated Result:\n");
+    printf("%s",str);
 }
-int main()
-{
-    char str1[100],str2[100];
-    printf("Enter string1:");
+
+int main() {
+    char str1[100], str2[100];
+
     input(str1);
-    printf("Enter string2:");
     input(str2);
-    concatenate_strings(str1,str2);
+
+    concatenate_strings(str1, str2);
+
     display(str1);
+
     return 0;
 }
